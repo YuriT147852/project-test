f7e63eb91c7a23ada7deccd3734e4deb66634d21
YuriT147852 <twy0171@gmail.com>
2024-04-13 12:40:27 +0800

    初始化專案

--- src/routes/api/v1/home/index.ts
+++ src/routes/api/v1/home/index.ts
@@ -1,23 +1,5 @@
 import { Router } from 'express';
-import culinaryRouter from './culinary';
-import newsRouter from './news';
-
 const router = Router();
 
-router.use(
-    /**
-     * #swagger.tags = ["Home/News - 最新消息"]
-     */
-    '/news',
-    newsRouter
-);
-
-router.use(
-    /**
-     * #swagger.tags = ["Home/Culinary - 美味佳餚"]
-     */
-    '/culinary',
-    culinaryRouter
-);
 
 export default router;

33196e8674a162a5afe3b916d0cbe2736dfe5aca
FroseWong <tibafrose@gmail.com>
2024-04-13 15:37:01 +0800

    frose test

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import Routes from '@/routes';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import Routes from "@/routes";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 app.use(Routes);
 
 // dawson testing ------
+// frose testing ------
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening on http://localhost:${process.env.PORT}`);
+  console.log(`listening on http://localhost:${process.env.PORT}`);
 });
